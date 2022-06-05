df875103c78af224f64438a1e2984d65b114f0e5
qthai <qthai@localhost>
2022-06-05 15:07:10 +0200

    Add archive preset settings

--- frontend/src/views/pages/archive/extra.js
+++ frontend/src/views/pages/archive/extra.js
@@ -8,6 +8,9 @@ export default Backbone.View.extend({
 
   events: {
     'change #layout': 'onLayoutChange',
+    'click button.preset-list-title-asc': 'onPresetListTitleAscClick',
+    'click button.preset-icon-title-asc': 'onPresetIconTitleAscClick',
+    'click button.preset-bubble-date-desc': 'onPresetBubbleDateDescClick',
     'click button.reset': 'onResetClick',
   },
 
@@ -25,6 +28,24 @@ export default Backbone.View.extend({
 
   onLayoutChange: function (e) {
     this.router.state.set({ layout: e.currentTarget.value });
+    this.router.dispatcher.trigger('archive:options');
+  },
+
+  onPresetListTitleAscClick: function (e) {
+    e.preventDefault();
+    this.router.state.set({ layout: 'layout-list', sort: 'title', sort_dir: 'asc' });
+    this.router.dispatcher.trigger('archive:options');
+  },
+
+  onPresetIconTitleAscClick: function (e) {
+    e.preventDefault();
+    this.router.state.set({ layout: 'layout-icon', sort: 'title', sort_dir: 'asc' });
+    this.router.dispatcher.trigger('archive:options');
+  },
+
+  onPresetBubbleDateDescClick: function (e) {
+    e.preventDefault();
+    this.router.state.set({ layout: 'layout-bubble', sort: 'date', sort_dir: 'desc' });
     this.router.dispatcher.trigger('archive:options');
   },
 
